816a7efbadfcb6af9d904df7e5589ba6d5cb210c
jaylalakiya <63226109+jaylalakiya@users.noreply.github.com>
2020-07-27 19:36:45 +0530

    start template setting after this commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# ! class MODEL
+# * title
+# * slug
+# * meta_description
+# * meta_keywords
+# * thumbnail
+# * overview
+# * content
+# * author
+# * tags
+# * created
+# * updated
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import index
+from blog.views import index, contact, about, post
 
 urlpatterns = [
     path('', index),
+    path('contact/', contact),
+    path('about/', about),
+    path('post/', post),
 ]
